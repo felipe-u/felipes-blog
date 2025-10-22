c2628c55717ead437d3f7ce01458434df5e0ea5a
Felipe U <felipe_uv@outlook.com>
2025-10-22 18:27:36 -0500

    feat (AboutPage): Create AboutPage component and create About.css for styling

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,3 +1,22 @@
+import '../styles/About.css'
+
 export function AboutPage() {
-  return <p>AboutPage works...</p>
+  return (
+    <div className='about-container'>
+      <h2>Who am I?</h2>
+      <div className='about-inner-container'>
+        <img
+          src='https://avatars.githubusercontent.com/u/88417696?v=4'
+          alt='My github profile picture'
+        />
+        <p>
+          My name is Felipe, I'm a simple person really. Someone who wants to
+          learn a lot about coding. <br /> <br />
+          I'm building this blog for practicing purposes but also for writing
+          random stuff I see in the world
+        </p>
+      </div>
+      <p>Thanks if you're reading this 🖤</p>
+    </div>
+  )
 }
